c1fefa63ed720a3d2166e28f6e4058a29cc30907
Roger Chen <rogerthechen@gmail.com>
2013-12-29 20:48:00 -0800

    Restructuring around Slim framework

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<? include_once('page.php'); ?>
+<?php
+
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim(array(
+    'templates.path' => './views'    
+));
+
+$app->get('/', function() use ($app) {
+    $app->render('page.php');
+});
+
+$app->run();
+
+?>
